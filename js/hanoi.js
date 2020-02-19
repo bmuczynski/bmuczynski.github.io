62bfcb6c9bff18c1dcb74e346138d99fd5c3ac8f
bmuczynski <b.muczynski@gmail.com>
2020-02-19 08:51:56 +0100

    schema, events

--- js/hanoi.js
+++ js/hanoi.js
@@ -1,132 +1,226 @@
 AFRAME.registerComponent('tower', {
     schema: {
-        stack: {type: 'array', default: []}
+        stack: {
+            type: 'array',
+            default: []
+        },
+        number: {
+            type: 'number'
+        }
     },
-    
-    init: function() {
-        var el = this.el;
 
-        el.addEventListener('click', function () {
-            
+    events: {
+        click: function (evt) {
+
+            var el = this.el;
             var pos = el.getAttribute('position');
             var helper = document.querySelector('[helper]').components.helper;
+            var currentTower = el.getAttribute('tower');
+            var data = this.data;
 
-            if(pos.x>0){
-                console.log("Prawa wieża");
-            } 
-            else if(pos.x == 0){
-                console.log("Środkowa wieża");
-            }
-            else if(pos.x<0) {
+            console.log("--------------click");
+            console.log(helper.data.stack[0].object3D.position.x);
+            console.log(helper.data.stack);
+            console.log(data.stack);
+        }
+      },
+
+    init: function () {
+        var el = this.el;
+        var data = this.data;
+        //poczatkowe zebranie wszystkich ringow na lewej wiezy
+        setTimeout(function() {
+            var pos = el.getAttribute('position');
+
+            if (pos.x < 0) 
+            {
                 console.log("Lewa wieża");
+    
+                var rings = document.querySelector('a-scene').querySelectorAll('[ring]');
+                
+                var tempStack = new Array(6);
+                
+                rings.forEach(element => {
+                    console.log(element.getAttribute('ring').size);
+                    tempStack[rings.length - 1 - element.getAttribute('ring').size] = element;
+                });
+
+                data.stack = tempStack; 
+                console.log(data.stack);
+ 
             }
 
+        }, 1000);
+
+        
+
+        //przemieszczanie kulki
+        el.addEventListener('mouseenter', function() {
+            var pos = el.object3D.position;
+            var helper = document.querySelector('[helper]').components.helper.data.stack;
+            // console.log(pos);
+            // helper.object3D.position.set(pos.x, pos.y + 0.42, pos.z);
+            // console.log(helper.object3D.position);
+            // console.log(helper);
             console.log(helper);
 
-            helper.popRing();
+            helper.forEach(element => {
+
+                element.object3D.position.set(pos.x, pos.y + 0.42, pos.z);
+                
+            });
+
+        });
+
+
+        el.addEventListener('click', function () {       
+
+            // var pos = el.getAttribute('position');
+            // var helper = document.querySelector('[helper]').components.helper;
+            // var currentTower = el.getAttribute('tower');
+            // var data = this.data;
+
+            // if (pos.x > 0) {
+            //     console.log("Prawa wieża");
+            // } else if (pos.x == 0) {
+            //     console.log("Środkowa wieża");
+            // } else if (pos.x < 0) {
+            //     console.log("Lewa wieża");
+            // }
+            // console.log("--------------click");
+            // console.log(helper.data.stack[0].object3D.position.x);
+            // console.log(helper.data.stack);
+            // console.log(data.stack);
+
+            //helper.popRing();
 
             //this.stack = tempStack;
-            
+
             //console.log(tempStack[0]);
             //console.log(tempStack[0].getAttribute('ring').isOnTop);
 
         });
 
+       
+
 
     },
 
-    /*update: function(){
+
+    update: function(oldData){
+        var data = this.data;
         var el = this.el;
-        var tempStack2 = [];
 
-        el.addEventListener('click', function(){
-            tempStack2 =  el.stack;
-            console.log(el.stack);
+        if (oldData.onClick !== data.onClick) {
+			el.addEventListener('click', function () {
+                el.setAttribute('tower', 'stack', data.onClick);
+                console.log("updateted");
+			});
+        }
+        
+        console.log("entered udpate");
+
             
-            console.log(tempStack2[0].getAttribute('ring'));
-        });
-
-
-    },
-
-    addRing: function(ring){
-        var ringToAdd = ring 
-        var sceneEl = document.querySelector('a-scene');
-        var tempStack = sceneEl.querySelectorAll('ring');
-        stack = tempStack;
-    }*/
-
+    }
 });
+/*update: function(){
+    var el = this.el;
+    var tempStack2 = [];
+
+    el.addEventListener('click', function(){
+        tempStack2 =  el.stack;
+        console.log(el.stack);
+        
+        console.log(tempStack2[0].getAttribute('ring'));
+    });
+
+
+},
+
+addRing: function(ring){
+    var ringToAdd = ring 
+    var sceneEl = document.querySelector('a-scene');
+    var tempStack = sceneEl.querySelectorAll('ring');
+    stack = tempStack;
+}*/
+
 
 AFRAME.registerComponent('ring', {
     schema: {
-        size: {type: 'number', default: 0},
-        isOnTop: {type: 'boolean', default: false}
-
-    },
-
-
-    init: function(){
-        var el = this.el;
-
-    },
-
-    // popRing: function(pos){
-    //     var helper = document.querySelectorAll('[helper]')[0];
-    //     var newPosition = helper.position;
-    //     var el = this.el;
-
-    //     console.log(helper);
-    //     console.log(newPosition)
-
-    //     el.setAttribute('position', newPosition);
-    // }
-
-
-
-});
-
-AFRAME.registerComponent('helper',{
-
-    
-    init: function(){
-        var el = this.el;
-
-        var rings = document.querySelector('a-scene').querySelectorAll('[ring]');
-        console.log(rings);
-        var tempStack = new Array(rings.length);
-
-        rings.forEach(element => {
-            
-            if(element.getAttribute('ring').size == 0) {
-                console.log(element.getAttribute('ring'));
-                console.log("Weszlo");   
-                element.setAttribute('ring', {isOnTop: true});
-                console.log(element.getAttribute('ring'));
-                //element.flushToDOM(); //aktualizuje wartości w htmlu
-            }
-            console.log(element.getAttribute('ring').size + " dodano do wiezy");
-            tempStack[rings.length - 1 - element.getAttribute('ring').size] = element;
-            console.log(tempStack);  
-            
-        });
-
-
-    },
-
-    popRing: function(){
-        
-        
-        console.log();
+        size: {
+            type: 'number',
+            default: 0
+        },
+        isOnTop: {
+            type: 'boolean',
+            default: false
+        }
     }
 });
 
-AFRAME.registerComponent('manager',{
+AFRAME.registerComponent('helper', {
     schema: {
-        isRingActive: {type: "boolean", default: false}
+        stack: {
+            type: 'array',
+            default: []
+        },
+        active: {
+            type: 'boolean',
+            default: true
+        }
     },
 
-    init: function(){
+    init: function () {
+        var el = this.el;
+
+        this.data.stack.push(el);
+    },
+
+    popRing: function () {
+
+        var el = this.el
+
+        console.log(el.object3D.position);
+
+        // if (this.data.active) {
+            
+            
+        //     //var ring = this.stack.pop();
+            
+            
+        //     var towers = document.querySelectorAll('[tower]');
+
+        //     towers.forEach(element => {
+        //         element.setAttribute('event-set__ring', {
+        //             _target: ring.getAttribute('id')
+        //         });
+        //         console.log(element.getAttribute('event-set__ring'));
+
+        //     })
+
+        //     //ring.setAttribute('position', el.object3D.position);
+
+        //     this.data.active = false;
+        //     console.log(ring);
+
+       //}
+
+
+
+
+
+    }
+});
+
+AFRAME.registerComponent('manager', {
+    schema: {
+        isRingActive: {
+            type: "boolean",
+            default: false
+        }
+    },
+
+    init: function () {
         var el = this.el;
         var data = this.data;
         var rings = document.querySelectorAll('ring');
@@ -134,6 +228,16 @@ AFRAME.registerComponent('manager',{
         var helper = document.querySelector('helper');
 
     }
-    
+
 
 });
+
+
+AFRAME.registerComponent('foo', {
+    events: {
+      click: function (evt) {
+        console.log('This entity was clicked!');
+        this.el.setAttribute('material', 'color', 'red');
+      }
+    }
+  });
