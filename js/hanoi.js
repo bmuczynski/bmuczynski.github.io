7791a8b956e8b5741959e1f6242cae1d53a7d8ed
bmuczynski <b.muczynski@gmail.com>
2020-02-19 22:25:16 +0100

    working mechanic

--- js/hanoi.js
+++ js/hanoi.js
@@ -17,140 +17,80 @@ AFRAME.registerComponent('tower', {
             var helper = document.querySelector('#helper').components.helper;
             var data = this.data;
 
-            // console.log("--------------click");
-            // console.log(helper.data.stack[0].object3D.position.x);
-            // console.log(helper.data.stack);
-            // console.log(data.stack);
-            
-            console.log(helper.data.active);
+            if (helper.data.active && data.stack.length != 0) {
+                helper.popRing(data.stack.pop());
+            } else if (!helper.data.active) {
+                if (data.stack.length == 0) {
+                    var height = 0.02 + data.stack.length * 0.08;
+                    var ring = helper.pushRing();
 
-            if(helper.data.active) helper.popRing(data.stack.pop());
+                    data.stack.push(ring);
+                    ring.object3D.position.set(pos.x, height, pos.z);
+                } else if (helper.data.stack[helper.data.stack.length - 1].components.ring.data.size < data.stack[data.stack.length - 1].components.ring.data.size) {
+                    var height = 0.02 + data.stack.length * 0.08;
+                    var ring = helper.pushRing();
+
+                    data.stack.push(ring);
+                    ring.object3D.position.set(pos.x, height, pos.z);
+                }
+            }
 
         }
-      },
+    },
 
 
     init: function () {
         var el = this.el;
         var data = this.data;
-       
+
         //poczatkowe zebranie wszystkich ringow na lewej wiezy
-        setTimeout(function() {
+        setTimeout(function () {
             var pos = el.getAttribute('position');
 
-            if (pos.x < 0) 
-            {
-                console.log("Lewa wieża");
-    
+            if (pos.x < 0) {
+
                 var rings = document.querySelector('a-scene').querySelectorAll('[ring]');
-                
+
                 var tempStack = new Array(6);
-                
+
                 rings.forEach(element => {
-                    console.log(element.getAttribute('ring').size);
+                   
                     tempStack[rings.length - 1 - element.getAttribute('ring').size] = element;
                 });
 
-                data.stack = tempStack; 
-                console.log(data.stack);
- 
+                data.stack = tempStack;
+
             }
 
         }, 1000);
 
-        
 
         //przemieszczanie kulki
-        el.addEventListener('mouseenter', function() {
+        el.addEventListener('mouseenter', function () {
 
             var pos = el.object3D.position;
             var helper = document.querySelector('[helper]').components.helper.data.stack;
-            // console.log(pos);
-            // helper.object3D.position.set(pos.x, pos.y + 0.42, pos.z);
-            // console.log(helper.object3D.position);
-            // console.log(helper);
-            console.log(helper);
-            console.log(data.stack);
             helper.forEach(element => {
 
-                element.object3D.position.set(pos.x, pos.y + 0.42, pos.z);
-                
+                element.object3D.position.set(pos.x, pos.y + 0.35, pos.z);
+
             });
 
         });
-
-
-        el.addEventListener('click', function () {       
-
-            // var pos = el.getAttribute('position');
-            // var helper = document.querySelector('[helper]').components.helper;
-            // var currentTower = el.getAttribute('tower');
-            // var data = this.data;
-
-            // if (pos.x > 0) {
-            //     console.log("Prawa wieża");
-            // } else if (pos.x == 0) {
-            //     console.log("Środkowa wieża");
-            // } else if (pos.x < 0) {
-            //     console.log("Lewa wieża");
-            // }
-            // console.log("--------------click");
-            // console.log(helper.data.stack[0].object3D.position.x);
-            // console.log(helper.data.stack);
-            // console.log(data.stack);
-
-            //helper.popRing();
-
-            //this.stack = tempStack;
-
-            //console.log(tempStack[0]);
-            //console.log(tempStack[0].getAttribute('ring').isOnTop);
-
-        });
-
-       
-
-
     },
 
 
-    update: function(oldData){
+    update: function (oldData) {
         var data = this.data;
         var el = this.el;
 
         if (oldData.onClick !== data.onClick) {
-			el.addEventListener('click', function () {
+            el.addEventListener('click', function () {
                 el.setAttribute('tower', 'stack', data.onClick);
-                console.log("updateted");
-			});
+            });
         }
-        
-        console.log("entered udpate");
-
-            
     }
 });
-/*update: function(){
-    var el = this.el;
-    var tempStack2 = [];
-
-    el.addEventListener('click', function(){
-        tempStack2 =  el.stack;
-        console.log(el.stack);
-        
-        console.log(tempStack2[0].getAttribute('ring'));
-    });
-
-
-},
-
-addRing: function(ring){
-    var ringToAdd = ring 
-    var sceneEl = document.querySelector('a-scene');
-    var tempStack = sceneEl.querySelectorAll('ring');
-    stack = tempStack;
-}*/
-
 
 AFRAME.registerComponent('ring', {
     schema: {
@@ -187,67 +127,21 @@ AFRAME.registerComponent('helper', {
 
         var el = this.el;
         var data = this.data;
+        var pos = el.getAttribute('position');
 
         data.stack.push(ring);
+        data.active = false;
 
-        console.log(data.stack);
-
-        // if (this.data.active) {
-            
-            
-        //     //var ring = this.stack.pop();
-            
-            
-        //     var towers = document.querySelectorAll('[tower]');
-
-        //     towers.forEach(element => {
-        //         element.setAttribute('event-set__ring', {
-        //             _target: ring.getAttribute('id')
-        //         });
-        //         console.log(element.getAttribute('event-set__ring'));
-
-        //     })
-
-        //     //ring.setAttribute('position', el.object3D.position);
-
-        //     this.data.active = false;
-        //     console.log(ring);
-
-       //}
-
-
-
-
-
-    }
-});
-
-AFRAME.registerComponent('manager', {
-    schema: {
-        isRingActive: {
-            type: "boolean",
-            default: false
-        }
+        ring.object3D.position.set(pos.x, pos.y, pos.z);
     },
 
-    init: function () {
+    pushRing: function () {
+
         var el = this.el;
         var data = this.data;
-        var rings = document.querySelectorAll('ring');
-        var towers = document.querySelectorAll('tower');
-        var helper = document.querySelector('helper');
+        var pos = el.getAttribute('position');
 
+        data.active = true;
+        return data.stack.pop();
     }
-
-
 });
-
-
-AFRAME.registerComponent('foo', {
-    events: {
-      click: function (evt) {
-        console.log('This entity was clicked!');
-        this.el.setAttribute('material', 'color', 'red');
-      }
-    }
-  });
---
+++ notes.rst
@@ -0,0 +1,15 @@
+Polozenia ringow
+================
+
+co 0.08 liczac od poziomu 0.02
+
+
++
+= 0.18
+= 0.10
+= 0.02
+
+
+tower box o pos.y = 0.35
+
+helper - pos.y 0.7
