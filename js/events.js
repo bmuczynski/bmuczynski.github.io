a9c37056db8b02ef0ef22b89dfc38dccc4cdbfe2
bmuczynski <b.muczynski@gmail.com>
2020-02-20 08:18:10 +0100

    event test

---
+++ js/events.js
@@ -0,0 +1,40 @@
+AFRAME.registerComponent('tower', {
+    
+
+    events:{
+
+    },
+
+    init:function(){
+        var el = this.el;
+        var tester = document.querySelector('#tester').components.tester;
+        el.addEventListener('click', function(){
+            tester.testFunction(el);
+        });
+        
+    },
+
+    update:function(){
+
+    },
+
+    testFunction:function(){
+
+        //var tester = document.querySelector('#tester');
+
+        console.log("Wywolano mnie");
+        //console.log(tester.components.tester);
+    }
+})
+
+AFRAME.registerComponent('tester',{
+    testFunction:function(element){
+
+        element.object3D.position.set(0, 1, -1);
+
+        //var tester = document.querySelector('#tester');
+
+        console.log("Wywolano mnie");
+        //console.log(tester.components.tester);
+    }
+})
