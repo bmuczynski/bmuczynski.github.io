99651edde5c2ee21348eb0606e66ee3a170be801
bmuczynski <b.muczynski@gmail.com>
2020-03-01 17:41:12 +0100

    final checks

--- js/hanoialt.js
+++ js/hanoialt.js
@@ -1,28 +1,24 @@
 AFRAME.registerComponent('vr-switcher', {
 
-init: function(){
-    var scene = document.querySelector('a-scene');
+    init: function () {
+        var scene = document.querySelector('a-scene');
 
-   scene.addEventListener('enter-vr', function () {
+        scene.addEventListener('enter-vr', function () {
+            if (AFRAME.utils.device.isMobile()) {
+                //alert("Mobile device!");
+                document.querySelector('#mouseCursor').setAttribute('material', 'visible: true');
+                document.querySelector('#mouseCursor').setAttribute('cursor', 'rayOrigin: entity');
+            }
+        });
 
-        console.log("entered vr");
-        if(AFRAME.utils.device.isMobile ()){
-            //alert("Mobile device!");
-            document.querySelector('#mouseCursor').setAttribute('material', 'visible: true');
-            document.querySelector('#mouseCursor').setAttribute('cursor', 'rayOrigin: entity');
-        }                                                                                   
-     });
+        scene.addEventListener('exit-vr', function () {
+            document.querySelector('#head').object3D.position.set(0, 1.6, 0);
+            document.querySelector('#mouseCursor').setAttribute('material', 'visible: false');
+            document.querySelector('#mouseCursor').setAttribute('cursor', 'rayOrigin: mouse');
 
-     scene.addEventListener('exit-vr', function () {
+        });
 
-        console.log("exited vr");
-        document.querySelector('#head').object3D.position.set(0, 1.6, 0);
-        document.querySelector('#mouseCursor').setAttribute('material', 'visible: false');
-        document.querySelector('#mouseCursor').setAttribute('cursor', 'rayOrigin: mouse');
-                                                                                        
-     });
-
-}
+    }
 
 });
 
@@ -37,7 +33,7 @@ AFRAME.registerComponent('tower', {
         number: {
             type: 'number'
         },
-        isStartingTower:{
+        isStartingTower: {
             type: 'boolean',
             default: false
         }
@@ -52,12 +48,14 @@ AFRAME.registerComponent('tower', {
 
         this.el.addEventListener("raycaster-intersected", evt => {
             this.raycaster = evt.detail.el;
-          });
+        });
+
         this.el.addEventListener("raycaster-intersected-cleared", evt => {
             this.raycaster = null;
-          });
+        });
+
         this.el.addEventListener("mouseenter", evt => {
-            
+
             var pos = el.object3D.position;
             var helper = document.querySelector('[helper]').components.helper.data.stack;
             helper.forEach(element => {
@@ -66,8 +64,8 @@ AFRAME.registerComponent('tower', {
 
             });
         });
-        this.el.addEventListener("mouseleave", evt => {
-        });
+
+        this.el.addEventListener("mouseleave", evt => {});
 
         //poczatkowe zebranie wszystkich ringow na lewej wiezy
         setTimeout(function () {
@@ -80,7 +78,7 @@ AFRAME.registerComponent('tower', {
                 var tempStack = new Array(6);
 
                 rings.forEach(element => {
-                   
+
                     tempStack[rings.length - 1 - element.getAttribute('ring').size] = element;
                 });
 
@@ -102,67 +100,60 @@ AFRAME.registerComponent('tower', {
 
         });
 
-        el.addEventListener('mousedown', function(){
-            
+        el.addEventListener('mousedown', function () {
+
             var pos = el.object3D.position;
             var helper = document.querySelector('#helper').components.helper;
 
-
             if (helper.data.active && data.stack.length != 0) {
                 helper.popRing(data.stack.pop());
-            } 
-            else if (!helper.data.active) {
-                
+            } else if (!helper.data.active) {
+
                 if (data.stack.length == 0) {
                     var height = 0.02 + data.stack.length * 0.08;
                     var ring = helper.pushRing();
 
                     data.stack.push(ring);
                     ring.object3D.position.set(pos.x, height, pos.z);
-                    console.log(data.stack);
-                    // if(!data.isStartingTower){
-                    //     if(data.stack.length == 2){
-                    //         console.log("WYGRAŁEŚ!!!!")
-                    //         document.querySelector('#particle').object3D.position.set(pos.x, 1.4, -1.6);
-                    //         document.querySelector('#particle').setAttribute('particle-system', {color: "#EF0000, #44CC00", 
-                    //         velocityValue: "0 15 0", 
-                    //         size: 0.2,
-                    //         accelerationValue: "0 -30 0",
-                    //         positionSpread: "0.1 0.1 0.1",
-                    //         particleCount: 500 });
-
-                    //     }
-                    // }
-                } 
-                
-                else if (helper.data.stack[helper.data.stack.length - 1].components.ring.data.size < data.stack[data.stack.length - 1].components.ring.data.size) {
+                    
+                } else if (helper.data.stack[helper.data.stack.length - 1].components.ring.data.size < data.stack[data.stack.length - 1].components.ring.data.size) {
                     var height = 0.02 + data.stack.length * 0.08;
                     var ring = helper.pushRing();
 
                     data.stack.push(ring);
                     ring.object3D.position.set(pos.x, height, pos.z);
-                    console.log(data.stack);
-                    if(!data.isStartingTower){
-                        if(data.stack.length == 3){
-                            console.log("WYGRAŁEŚ!!!!")
-                            document.querySelector('#particle').object3D.position.set(pos.x, 0, -15);
-                            document.querySelector('#particle').setAttribute('particle-system', {color: "#EF0000, #44CC00", 
-                            velocityValue: "0 25 0", 
-                            velocitySpread: "10 7.5 10",
-                            size: 1,
-                            accelerationSpread: "10 0 10",
-                            accelerationValue: "0 -10 0",
-                            positionSpread: "0.1 0.1 0.1",
-                            particleCount: 1000,
-                            dragValue: 0.2,
-                            maxAge: 6,
-                            blending: 2 });
-
+                    
+                    if (!data.isStartingTower) {
+                        if (data.stack.length == 6) {
+                            el.components.tower.gameWon();
                         }
                     }
                 }
             }
         });
+    },
+
+    gameWon: function () {
+        
+        document.querySelector('#particle').object3D.position.set(0, 0, -15);
+        document.querySelector('#particle').setAttribute('particle-system', {
+            color: "#EF0000, #44CC00",
+            velocityValue: "0 25 0",
+            velocitySpread: "10 7.5 10",
+            size: 1,
+            accelerationSpread: "10 0 10",
+            accelerationValue: "0 -10 0",
+            positionSpread: "0.1 0.1 0.1",
+            particleCount: 1000,
+            dragValue: 0.2,
+            maxAge: 6,
+            blending: 2
+        });
+
+        setTimeout(function () {
+            document.querySelector('#hanoiTower').object3D.visible = false;
+
+        }, 500);
     },
 });
 
@@ -217,5 +208,7 @@ AFRAME.registerComponent('helper', {
 
         data.active = true;
         return data.stack.pop();
-    }
+    },
+
+
 });
